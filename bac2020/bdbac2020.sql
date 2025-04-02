56092b8bf1a475ce138529f18563ccdadaa54597
My name bahaeddineKRAYEM <baharbaharbahar194@gmail.com>
2025-04-02 21:56:02 +0100

    Update bdbac2020.sql

--- bac2020/bdbac2020.sql
+++ bac2020/bdbac2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 30 déc. 2023 à 13:46
+-- Généré le : mer. 02 avr. 2025 à 22:55
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -28,12 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `client` (
-  `tel` varchar(8) NOT NULL,
-  `nom` varchar(25) NOT NULL,
-  `prenom` varchar(25) NOT NULL,
-  `adresse` varchar(100) NOT NULL,
-  `motpasse` varchar(6) NOT NULL
+  `Tel` varchar(8) NOT NULL,
+  `Nom` varchar(25) NOT NULL,
+  `Prenom` varchar(25) NOT NULL,
+  `Adresse` varchar(100) NOT NULL,
+  `MotPass` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`Tel`, `Nom`, `Prenom`, `Adresse`, `MotPass`) VALUES
+('29565671', 'krayem', 'bahaeddine', 'Cité El Mahrajène', '123456'),
+('29565673', 'krayem', 'bahaeddine', '', '123456'),
+('29565675', 'krayem', 'bahaeddine', '', '654123');
 
 -- --------------------------------------------------------
 
@@ -42,11 +51,21 @@ CREATE TABLE `client` (
 --
 
 CREATE TABLE `commande` (
-  `idpizza` varchar(3) NOT NULL,
-  `tel` varchar(8) NOT NULL,
-  `datecmd` datetime NOT NULL,
-  `qtecmd` int(11) NOT NULL
+  `IdPizza` varchar(3) NOT NULL,
+  `Tel` varchar(8) NOT NULL,
+  `DateCmd` datetime NOT NULL,
+  `QteCmd` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`IdPizza`, `Tel`, `DateCmd`, `QteCmd`) VALUES
+('Fmr', '29565671', '0000-00-00 00:00:00', 2),
+('Fmr', '29565671', '2025-04-02 00:00:00', 2),
+('Nap', '29565673', '0000-00-00 00:00:00', 2),
+('Nap', '29565673', '2025-04-02 00:00:00', 2);
 
 -- --------------------------------------------------------
 
@@ -55,22 +74,22 @@ CREATE TABLE `commande` (
 --
 
 CREATE TABLE `pizza` (
-  `idpizza` varchar(3) NOT NULL,
-  `nompizza` varchar(30) NOT NULL,
-  `details` varchar(200) NOT NULL,
-  `prix` decimal(6,3) NOT NULL
+  `IdPizza` varchar(3) NOT NULL,
+  `NomPizza` varchar(30) NOT NULL,
+  `Details` varchar(200) NOT NULL,
+  `Prix` decimal(6,3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `pizza`
 --
 
-INSERT INTO `pizza` (`idpizza`, `nompizza`, `details`, `prix`) VALUES
-('fmr', 'fruits de mer', 'tomate,mozzarella, fruits de mer ', 18.500),
-('mgh', 'margherita', 'tomate,mozzarella,basilic', 8.000),
-('nap', 'napolitaine', 'tomate,mozzarelle,thon,anchois', 12.000),
-('nep', 'neptune', 'tomate,mozzarella,thon', 10.500),
-('roy', 'royale', 'tomate,mozzarella,jambon,champignon', 14.500);
+INSERT INTO `pizza` (`IdPizza`, `NomPizza`, `Details`, `Prix`) VALUES
+('Fmr', 'Fruits de mer', 'Tomate, Mozzarella, Fruits de mer', 18.500),
+('Mgh', 'Margherita', 'Tomate, Mozzarella, Basilic', 8.000),
+('Nap', 'Napolitaine ', 'Tomate, Mozzarella, Thon, Anchois', 12.000),
+('Nep', 'Neptune', 'Tomate, Mozzarella, Thon ', 10.500),
+('Roy', 'Royale ', 'Tomate, Mozzarella, Jambon, Champignon', 14.500);
 
 --
 -- Index pour les tables déchargées
@@ -80,19 +99,32 @@ INSERT INTO `pizza` (`idpizza`, `nompizza`, `details`, `prix`) VALUES
 -- Index pour la table `client`
 --
 ALTER TABLE `client`
-  ADD PRIMARY KEY (`tel`);
+  ADD PRIMARY KEY (`Tel`);
 
 --
 -- Index pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD PRIMARY KEY (`idpizza`,`tel`,`datecmd`);
+  ADD PRIMARY KEY (`IdPizza`,`Tel`,`DateCmd`),
+  ADD KEY `IdPizza` (`IdPizza`,`Tel`),
+  ADD KEY `fh` (`Tel`);
 
 --
 -- Index pour la table `pizza`
 --
 ALTER TABLE `pizza`
-  ADD PRIMARY KEY (`idpizza`);
+  ADD PRIMARY KEY (`IdPizza`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `fh` FOREIGN KEY (`Tel`) REFERENCES `client` (`Tel`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk` FOREIGN KEY (`IdPizza`) REFERENCES `pizza` (`IdPizza`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
