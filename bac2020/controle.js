07723da4256b28e669674081cdf0a71cf2aaacbd
My name bahaeddineKRAYEM <baharbaharbahar194@gmail.com>
2025-04-02 21:58:06 +0100

    Update Biblio.js

--- bac2020/controle.js
+++ bac2020/controle.js
@@ -1,93 +1,57 @@
-function verif1(){
-    const nom=document.getElementById("nom").value;
-    if(!alpha(nom)||nom.length<=3){
-        alert ("Chaîne alphabétique de taille minimale égale à 3");
-        return false;
-    }
-    const prenom=document.getElementById("prenom").value;
-    if(!alpha(prenom)||prenom.length<=3){
-        alert ("Chaîne alphabétique de taille minimale égale à 3!!!!");
-        return false;
-    }
-    const tel=document.getElementById("tel").value;
-    if(isNaN(tel)||tel.length!=8 || tel[0]<1 ){
-        alert ( "Numéro de téléphone invalide, saisissez un numéro de 8 chiffres ");
-        return false;
-    }
-    const adresse=document.getElementById("adresse").value;
-    if(!alpha(adresse)||adresse.length!=10){
-        alert ("Adresse non valide!!!");
-        return false;
-    }
-    const motdepasse=document.getElementById("motdepasse").value;
-    if(!alpha(motdepasse)||motdepasse.length!=" "){
-        alert ("Mot de passe doit contenir au moins une majuscule et une minuscule ,et comporter au moins 6 ");
-        return false;
-    }
-    const cdmddp=document.getElementById("cdmddp").value;
-    if(cdmddp !=motdepasse){
-        alert ("Mot de passe et confirmation ne correspondent pas!");
-        return false;
-    }
-}
-function alpha(ch){
-    for(let i=0;i<ch.length;i++){
-        if(ch[i]<"A" && ch[i]>"Z"&ch[i]!=""){
-            test=false;
+function alpha(ch) {
+    ch = ch.toUpperCase();
+    for (let i = 0; i < ch.length; i++) {
+        if (ch[i] < "A" || ch[i] > "Z" && ch[i] < "a" || ch[i] > "z") {
+            return false;
         }
-        return true;
+    } return true;
+}
+function Verif1() {
+    let nom = document.getElementById("nom").value;
+    if (nom.length < 3 || !alpha(nom)) {
+        alert("Nom invalide");
+        return false;
+    }
+    let prenom = document.getElementById("prenom").value;
+    if (prenom.length < 3 || !alpha(prenom)) {
+        alert("Prenom invalide");
+        return false;
+    }
+    let tel = document.getElementById("tel").value;
+    if (tel.length != 8||isNaN(tel)|| Number(tel[0])<1) {
+        alert("Tel invalide");
+        return false;
+    }
+    let mdp = document.getElementById("mdp").value;
+    if (mdp.length != 6||isNaN(mdp) && mdp[i]!=" ") {
+        alert("Mot de passe  invalide");
+        return false;
+    }
+    let cmdp = document.getElementById("cmdp").value;
+    if (mdp!=cmdp) {
+        alert("comfirmation du Mot de passe  invalide");
+        return false;
     }
 }
-function num(ch){
-    for(let i=0;i<ch.length;i++){
-        if(ch[i]<"0" && ch[i]>"9"&ch[i]!=""){
-            test=false;
-        }
-        return true;
+function Verif2(){
+    let pizza=document.getElementById("pizza").selectedIndex;
+    if (pizza==0){
+        alert ("pizza inavalide");
+        return false;
+    }
+    let quantity=document.getElementById("quantity").value;
+    if (Number(quantity)<1 || Number(quantity)>5){
+        alert ("quantity inavalide");
+        return false;
+    }
+    let tel = document.getElementById("tel").value;
+    if (tel.length != 8||isNaN(tel)|| Number(tel[0])<1) {
+        alert("Tel invalide");
+        return false;
+    }
+    let mdp = document.getElementById("mdp").value;
+    if (mdp.length != 6||isNaN(mdp) && mdp[i]!=" ") {
+        alert("Mot de passe  invalide");
+        return false;
     }
 }
-function verif2(){
-    const pizza=document.getElementById("pizza").selectedIndex;
-    if(pizza==0){
-        alert("Vous n'avez pas choisi la pizza");
-        return false;
-    }
-    const quantity=document.getElementById("quantity").value;
-    if(Number(quantity)<1||Number(quantity)>5){
-        alert("La quantité doit être comprise entre 1 et 5");
-        return false;
-    }
-    const tel=document.getElementById("tel").value;
-    if(isNaN(tel)||tel.length!=8 || tel[0]<1 ){
-        alert ( "Numéro de téléphone invalide, saisissez un numéro de 8 chiffres ");
-        return false;
-    }
-    const motdepasse=document.getElementById("motdepasse").value;
-    if(isNaN(motdepasse)||motdepasse.length!=6 || motdepasse[i]!=" "){
-        alert("Le mot de passe doit contenir que des lettres et des espaces");
-        return false;
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function annuler {
-//     Document.getElementById("email")
-//     Document.getElementById("password")
-//     Document.getElementById("genre"). selectIndex
-//     Document.getElementById("r1")
-//     Document.getElementById("r2")
-//     Document.getElementById("r3")
-// }
